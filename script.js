50756d2edbd7e26423f5962f1b85aba2130798f7
Thomas Rager <tomrager@yahoo.com>
2020-10-16 12:26:16 -0400

    trying to figure out how to display the dates for the 5-day weather forecast

--- script.js
+++ script.js
@@ -1,26 +1,79 @@
-// This is our API key. Add your own API key between the ""
-var APIKey = '166a433c57516f51dfab1f7edaed8413';
-// Here we are building the URL we need to query the database
-var queryURL =
-  'https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=' +
-  APIKey;
-// We then created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: 'GET',
-}).then(function (response) {
-  console.log('queryURL:', queryURL);
-  console.log('response:', response);
-  $('.city').html('<h1>' + response.name + 'Weather Details</h1>');
-  $('.wind').html('<h1>' + response.wind.speed + ' Wind speed</h1>');
-  $('.humidity').html('<h1>' + response.main.humidity + ' Humidity</h1>');
-  // Convert the temp to fahrenheit
-  var tempF = (response.main.temp - 273.15) * 1.8 + 32;
-  // add temp content to html
-  $('.temp').text('Temperature (K) ' + response.main.temp);
-  $('.tempF').text('Temperature (F) ' + tempF.toFixed(2));
-  // Log the data in the console as well
-  console.log('Wind Speed: ' + response.wind.speed);
-  console.log('Humidity: ' + response.main.humidity);
-  console.log('Temperature (F): ' + tempF);
-});
+$(document).ready(function () {
+  const APIKey = "166a433c57516f51dfab1f7edaed8413";
+  var queryURL;
+  var today=moment().format("(M/D/YYYY)");
+  var iconCode;
+  var iconImg;
+
+  $(".search-button").on("click", function(event){
+      event.preventDefault();
+      $(".current-conditions").css("border","solid");
+      var citySearched=$(".searched-city").val();
+      queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      citySearched +
+      "&appid=" +
+      APIKey;
+      searchCall(queryURL);
+      forecastCall(citySearched);
+  });
+
+  function uvRender(cityLat,cityLon){
+      queryURL="https://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat+ "&lon=" + cityLon + "&appid=" + APIKey;
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function(response) {
+        console.log(response);
+        var uvIndex=response.value;
+        console.log(uvIndex);
+        $('.uv-index').text('UV Index: ');
+        $('.uv-value').text(uvIndex);
+        if (uvIndex < 3){
+          $('.uv-value').css("background-color", "green");
+        }else if (uvIndex >= 3 && uvIndex < 6){
+          $('.uv-value').css("background-color", "yellow");
+        }else if (uvIndex >=6 && uvIndex < 8){
+          $('.uv-value').css("background-color", "orange");
+        }else if (uvIndex >=8 && uvIndex <= 10){
+          $('.uv-value').css("background-color", "red");
+        } else {$('.uv-value').css("background-color", "purple");}
+    });
+  }
+
+  function forecastCall(citySearched){
+    var cityForecasted = citySearched;
+    queryURL="https://api.openweathermap.org/data/2.5/forecast?q="+cityForecasted+"&appid="+APIKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response){
+      console.log(response);
+      $(".day").each(function (daysForecasted) {
+        $(".day").css({"width":"40px", "height":"40px", "background-color":"#00BFFF"});
+        let today = moment();
+        today.add(daysForecasted,'days');
+        let date = moment(day).format("M/D/YYYY");
+        $(this).html(date);
+      })
+    })
+  }  
+
+  function searchCall(queryURL){
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      iconCode=response.weather[0].icon;
+      iconImg= '<img src=http://openweathermap.org/img/wn/'+ iconCode + '.png alt="weather icon"';
+      $('.city-date-cond').html('<h1>' + response.name +' '+ today + ' '+ iconImg +'</h1>');
+      var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+      $('.temperature').text('Temperature: ' + tempF.toFixed(1) +'°F');
+      $('.humidity').text('Humidity: ' + (response.main.humidity)+'%');
+      $('.wind-speed').text('Wind Speed: '+(response.wind.speed) + ' MPH');
+      var cityLat = response.coord.lat;
+      var cityLon = response.coord.lon;
+      uvRender(cityLat,cityLon);
+    })
+  }    
+})
